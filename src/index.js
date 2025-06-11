8f88e983b302559164618533d5546d3467c21e5a
rdevang71 <rdevang71@gmail.com>
2025-06-11 22:19:14 +0530

    setting up for API response

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import mongoose from "mongoose"; 
-import { DB_NAME } from "./constants";
+//  require('dotenv').config({path:'./env'})
+import connectDB from "./db/index.js";
+
+
+import dotenv from "dotenv";
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log(`Error in connectiong databae ERROR:`,error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at PORT:
+            ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Mongodb connection ERROR :",error)
+})
+
+
+
+// import mongoose from "mongoose"; 
+// import { DB_NAME } from "./constants";
+
 
 // import express from "express";
 // const app = express();
