c6f634a0de80da4752db1415e1a45a559d4d4007
rdevang71 <rdevang71@gmail.com>
2025-06-13 19:58:54 +0530

    adding encryption to the password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose , {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -52,5 +54,13 @@ const userSchema = new Schema(
  }
 
 )
+
+userSchema.pre("save",async function  (next ) {
+    if(!this.isModified("password")){
+      return next();
+    }
+    this.password= bcrypt.hash(this.password , 10)
+    next()
+})
 
 export const User = mongoose.model("User",userSchema)
