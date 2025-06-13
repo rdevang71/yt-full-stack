a9556830b8679b2379e0b28bbfc8ac6712093a88
rdevang71 <rdevang71@gmail.com>
2025-06-13 21:42:31 +0530

    adding tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
         lowercase: true,
         trim: true,
     },
-    fullname:{
+    fullName:{
         type: String,
         required : true,
         trim: true,
@@ -67,6 +67,33 @@ userSchema.methods.isPasswordCorrect = async function
 (password)
 {
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    }, process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIN: process.nextTick.ACCESS_TOKEN_EXPIRY,
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    }, process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIN: process.nextTick.REFRESH_TOKEN_EXPIRY,
+    }
+)
 }
 
 export const User = mongoose.model("User",userSchema)
