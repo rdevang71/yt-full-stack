ca17f301e728e3246d0e3d288421bae2cfc1f5d0
rdevang71 <rdevang71@gmail.com>
2025-06-18 11:07:42 +0530

    adding user route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //  require('dotenv').config({path:'./env'})
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 
 import dotenv from "dotenv";
 dotenv.config({
@@ -15,8 +15,7 @@ connectDB()
         throw error;
     })
     app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running at PORT:
-            ${process.env.PORT}`)
+        console.log(`Server is running at PORT:${process.env.PORT}`)
     })
 })
 .catch((error)=>{
