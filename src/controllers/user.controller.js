f0f094998b525a8340f63a8f02f0df40ec3a2672
rdevang71 <rdevang71@gmail.com>
2025-06-19 11:25:44 +0530

    steps for userregistration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message: "ok"
-    })
+      //get user details from frontend
+      //add validation check
+      // check if user already exist : username , email
+      // check for images : for avtar and coverimage 
+      // upload them on cloudinary, avtar
+      // create user object - create entry in db
+      // remove password and refresh token field from response 
+      // check for user creation : return response 
+
 })
 
 export {registerUser};
