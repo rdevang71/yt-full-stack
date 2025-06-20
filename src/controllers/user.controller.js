97c5595e9a9cebfdb91b9519778ef40502168695
rdevang71 <rdevang71@gmail.com>
2025-06-20 20:12:45 +0530

    adding multer middleware in user route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {apiError} from "../utils/apiErrors.js"
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler(async (req,res)=>{
       //get user details from frontend
+      const {username , email , fullName ,password}=req.body
+
       //add validation check
+      // if(fullName==="")
+      // {
+      //       throw new apiError(400,"Full Name is required !! !")
+      // }
+      if([username, email,fullName,password].some((field)=>{
+            field?.trim()===""
+      }))
+      {
+            throw new apiError(400, "All Fields Are Required!!")
+      }
       // check if user already exist : username , email
       // check for images : for avtar and coverimage 
       // upload them on cloudinary, avtar
