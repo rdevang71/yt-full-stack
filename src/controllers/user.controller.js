36f01a89ec90b87a64830a2f95d00775a64bc096
rdevang71 <rdevang71@gmail.com>
2025-07-01 12:13:25 +0530

    updating routes for refreshAccessToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -219,4 +219,8 @@ const refreshAccessToken = asyncHandler(async ( req,res)=>{
      }
 })
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser,
+   loginUser,
+    logoutUser,
+  refreshAccessToken
+ };
