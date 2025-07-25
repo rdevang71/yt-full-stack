aefd624164d06d4c80c9635a628fecbabe17a477
rdevang71 <rdevang71@gmail.com>
2025-07-25 11:43:19 +0530

    feat: add update comment functionality with validation and authorization checks

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -31,7 +31,6 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
   const totalComments = await Comment.countDocuments({ video: videoId });
 
-
   return res.status(200).json(
     new apiResponse(
       200,
@@ -50,35 +49,65 @@ const getVideoComments = asyncHandler(async (req, res) => {
 });
 
 const addComment = asyncHandler(async (req, res) => {
-    const { content, videoId } = req.body;
+  const { content, videoId } = req.body;
 
-    if(!content){
-        throw new apiError(400,"Content of the comment is required")
-    }
+  if (!content) {
+    throw new apiError(400, "Content of the comment is required");
+  }
 
-    const comment = await Comment.create({
-        content,
-        video: videoId,
-        owner : req.user._id
-    })
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user._id,
+  });
 
-    const addedComment = await Comment.findById(comment._id).populate(
-        "owner",
-        "username avatar"
+  const addedComment = await Comment.findById(comment._id).populate(
+    "owner",
+    "username avatar"
+  );
+
+  if (!addedComment) {
+    throw new apiError(
+      500,
+      " Something went wrong when publishing the comment"
     );
-
-    if(!addedComment){
-        throw new apiError(500, " Something went wrong when publishing the comment")
-    }
-    return res
+  }
+  return res
     .status(201)
-    .json(
-        new apiResponse(200 , addedComment, " Comment Added Successfully")
-    );
+    .json(new apiResponse(200, addedComment, " Comment Added Successfully"));
+});
 
-})
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const {updatedContent} = req.body;
 
+  if (!updatedContent) {
+    throw new apiError(400, "new content is required to update the comment ");
+  }
 
-export {getVideoComments,
-    addComment
-} ;
+  if (!mongoose.Types.ObjectId.isValid(commentId)) {
+    throw new apiError(404, "Invalid Comment id ");
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new apiError(400, "Comment not found");
+  }
+
+  if (comment.owner.toString() !== req.user._id.toString()) {
+    throw new apiError(403, "You are not authorized to update this Comment");
+  }
+
+  comment.content = updatedContent;
+
+  await comment.save();
+
+  await comment.populate("owner", "username avatar");
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, comment, "comment Updated Successfully"));
+});
+
+export { getVideoComments, addComment , updateComment};
--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getVideoComments, addComment } from "../controllers/comments.controller.js";
+import { getVideoComments, addComment, updateComment } from "../controllers/comments.controller.js";
 
 const router =Router();
 
 router.route("/get-comments").get(verifyJWT , getVideoComments)
 router.route("/add-comment").post(verifyJWT , addComment)
+router.route("/update-comment/:commentId").patch(verifyJWT , updateComment)
 
 export default router;
