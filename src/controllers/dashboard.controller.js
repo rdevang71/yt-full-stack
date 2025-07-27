564451969181d2c2c860f5e022669b59dae7d3cc
rdevang71 <rdevang71@gmail.com>
2025-07-27 11:19:25 +0530

    fix: update import paths for subscription and error utility modules

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
-import { Subscription } from "../models/subscription.model.js";
+import { Subscription } from "../models/subscriptions.model.js";
 import { Like } from "../models/like.model.js";
-import { apiError } from "../utils/apiError.js";
+import { apiError } from "../utils/apiErrors.js";
+import { Tweet } from "../models/tweet.model.js";
+import { Comment } from "../models/comment.model.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
